274cdb431e29641cebefb2f1e6c6818c3a1c4d11
Enamul Hasan <syl.enamul@gmail.com>
2024-02-08 14:23:28 +0600

    book model added

--- django_app/books/models.py
+++ django_app/books/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+class Books(models.Model):
+    title = models.CharField()
+    author = models.CharField()
+    area = models.CharField()
+    genre = models.CharField()
+    price = models.FloatField()
+
