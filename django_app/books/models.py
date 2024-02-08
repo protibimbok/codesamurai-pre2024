8e4fed0587abaf34350b1ef9b0a25f59243c862f
Enamul Hasan <syl.enamul@gmail.com>
2024-02-08 14:30:56 +0600

    changed book model

--- django_app/books/models.py
+++ django_app/books/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Books(models.Model):
-    title = models.CharField()
-    author = models.CharField()
-    area = models.CharField()
-    genre = models.CharField()
+    title = models.CharField(max_length=5000)
+    author = models.CharField(max_length=5000)
+    area = models.CharField(max_length=5000)
+    genre = models.CharField(max_length=5000)
     price = models.FloatField()
 
