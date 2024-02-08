6bbd3dc7a9098b6f63915c708c59b2d700a689f3
Sakibur Rahman Khan <sakib.saad.khan@gmail.com>
2024-02-08 14:33:54 +0600

    fix books migrations

--- django_app/books/models.py
+++ django_app/books/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Books(models.Model):
-    title = models.CharField(max_length=5000)
-    author = models.CharField(max_length=5000)
-    area = models.CharField(max_length=5000)
-    genre = models.CharField(max_length=5000)
+    title = models.TextField()
+    author = models.TextField()
+    area = models.TextField()
+    genre = models.TextField()
     price = models.FloatField()
 
