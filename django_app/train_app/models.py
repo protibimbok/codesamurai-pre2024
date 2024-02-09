4407384d2d38ca56bf72212e03b18c533bd8ab67
Enamul Hasan <syl.enamul@gmail.com>
2024-02-09 16:09:05 +0600

    trainapp

--- django_app/train_app/models.py
+++ django_app/train_app/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
-class Book(models.Model):
+# Create your models here.
+class User(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.TextField()
     author = models.TextField()
     genre = models.TextField()
     price = models.FloatField()
-
-
-    
-
